490643e59bf08f01d3d2c86cb9ed4c7db79449fe
yu hsiang <lin@test.com>
2025-08-21 22:03:17 +0800

    Adjust ui style

--- src/app/shared/components/loading-overlay/loading-overlay.ts
+++ src/app/shared/components/loading-overlay/loading-overlay.ts
@@ -10,5 +10,5 @@ import { CommonModule } from '@angular/common';
 })
 export class LoadingOverlay {
   @Input() isLoading: boolean = false;
-  @Input() message: string = '載入中…';
+  @Input() message: string = 'Loading…';
 }
